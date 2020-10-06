8669f55822918432f23a4a95ad0a41c23420e592
Maria Zebroff <bastinda18@yahoo.ca>
2020-10-06 12:48:31 -0700

    models and routes were added

---
+++ routes/api/pets.js
@@ -0,0 +1,11 @@
+const express  = require('express');
+
+const router = express.Router();
+
+//@route  GET api/pets
+//@desc   Test route
+//@access Public
+router.get('/', (req, res)=>res.send('Pets Route'));
+
+
+module.exports = router;
